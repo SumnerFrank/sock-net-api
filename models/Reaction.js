4dfb9eeafe0d53f0aaf77342ea5b4005a383ef98
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-18 11:15:09 -0400

    reaction model updated code

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,11 +2,31 @@ const { Schema, model} = require('mongoose');
 
 const reactionSchema = new Schema(
     {
-        reactionId: {},
-        reactionBody: {},
-        username: {},
-        createdAt: {}
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false
     }
-)
+);
 
 module.exports = Reaction;
