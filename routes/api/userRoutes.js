e6390d20ab7717d44f8ba759b4dad09d574ba179
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-18 10:43:24 -0400

    initial userRoutes code

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const {
+  getUsers,
+  getOneUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
+} = require('../../controllers/userController.js');
+
+router.route('/')
+.get(getUsers)
+.post(createUser);
+
+router
+  .route('/:userId')
+  .get(getOneUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
+module.exports = router;
