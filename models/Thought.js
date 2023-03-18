049a7d3db978aa48a5116e57fe1c66ae922215d2
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-18 11:17:59 -0400

    thought model updated code

--- models/Thought.js
+++ models/Thought.js
@@ -2,11 +2,25 @@ const { Schema, model} = require('mongoose');
 
 const thoughtSchema = new Schema(
     {
-        thoughtText: {},
-        createdAt: {},
-        username: {},
-        userId: {},
-        reactions: {}
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        userId: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
     }
 )
 
