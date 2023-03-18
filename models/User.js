28d47ec1042d014d6303500f92792af9b2ba02cb
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-18 11:31:57 -0400

    user model updated code

--- models/User.js
+++ models/User.js
@@ -2,11 +2,44 @@ const { Schema, model} = require('mongoose');
 
 const userSchema = new Schema(
     {
-        username: {},
-        email: {},
-        thoughts: {},
-        friends: {}
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Please enter a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+            id: false
     }
-)
+);
+
+userSchema
+.virtuals()
+.get()
 
 module.exports = User;
