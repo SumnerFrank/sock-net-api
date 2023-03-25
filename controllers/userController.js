56cd14ac464ce6e61591ef368303f71913cfa8a5
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-25 09:24:33 -0400

    wrote template for user controllers to be filled in

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const { User, Thought } = require('../models/index');
+
+const userController = {
+    getUsers({}){
+
+    },
+    
+    getOneUser({}){
+
+    },
+    
+    createUser({}){
+
+    },
+    
+    updateUser({}){
+
+    },
+    
+    deleteUser({}){
+        
+    },
+    
+    addFriend({}){
+        
+    },
+    
+    removeFriend({}){
+        
+    }
+}
+
+module.exports = UserController;
