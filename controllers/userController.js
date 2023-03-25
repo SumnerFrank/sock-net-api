d0f9113cf19ff7335b4e8f410a4eb4d713e11788
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-25 10:00:10 -0400

    began filling in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,40 @@
 const { User, Thought } = require('../models/index');
 
 const userController = {
-    getUsers({}){
-
+    getUsers(req, res){
+        User.find({})
+        .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v -thoughts'})
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            return res.status(400).json(err)
+        });
     },
     
-    getOneUser({}){
-
+    getOneUser({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user with this ID' });;
+                return;
+            }
+            return res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            return res.json(400).json(err);
+        })
     },
     
-    createUser({}){
-
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .cacth(err => res.status(400).json(err))
     },
     
     updateUser({}){
