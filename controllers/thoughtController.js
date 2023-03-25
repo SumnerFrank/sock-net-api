fc688e5ec7fd0a65bd17ca9f6a4d9d380e56f228
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-25 08:59:28 -0400

    began writing thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,27 @@
 const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getThoughts(req, res) {
+        Thought.find({})
+        .select()
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            return res.status(400).json(err);
+        })
+    },
+    getOneThought(req, res) {
+        Thought.fineOne({_id: params.thoughtId })
+        .select()
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought from this ID'})
+            }
+            return res.json(dbThoughtData)
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(400).json(err);
+        })
+    }
+}
